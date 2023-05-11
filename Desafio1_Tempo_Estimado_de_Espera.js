7604494926d95af036e0193fd36529ed9b2f53c6
Victor Alexandre Vieira de Matos <112444309+VictorAVMatos@users.noreply.github.com>
2023-05-11 19:13:38 -0300

    Add files via upload

---
+++ Desafio1_Tempo_Estimado_de_Espera.js
@@ -0,0 +1,5 @@
+const nomeRestaurante = gets('Giraffas');
+const tempoEstimadoEntrega = parseInt(gets(35));
+
+const mensagem = `O restaurante ${nomeRestaurante} entrega em ${tempoEstimadoEntrega} minutos.`;
+print(mensagem);
